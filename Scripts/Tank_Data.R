7d65f18725bbfa25e2aae8eb282c8f4834bd64e2
Maya Powell <maya62928@gmail.com>
2026-03-08 18:54:09 -1000

    updating graph tank data

--- Scripts/Tank_Data.R
+++ Scripts/Tank_Data.R
@@ -21,28 +21,29 @@ td <- td %>%
 
 temp <- ggplot(td, aes(x = date_time, y = temp, color = as.factor(tank), shape = coral_no)) +
   geom_point() +
-  theme(axis.text.x = element_text(angle = 90)) +
-  theme_bw(base_size = 20)
+  theme_bw(base_size = 20) +
+  theme(axis.text.x = element_text(angle = 90))
 
 pH <- ggplot(td, aes(x = date_time, y = pH, color = as.factor(tank), shape = coral_no)) +
   geom_point() +
-  theme(axis.text.x = element_text(angle = 90)) +
-  theme_bw(base_size = 20)
+  theme_bw(base_size = 20) +
+  theme(axis.text.x = element_text(angle = 90))
 
 DO <- ggplot(td, aes(x = date_time, y = DO, color = as.factor(tank), shape = coral_no)) +
   geom_point() +
-  theme(axis.text.x = element_text(angle = 90)) +
-  theme_bw(base_size = 20)
+  theme_bw(base_size = 20) +
+  theme(axis.text.x = element_text(angle = 90))
 
 sal <- ggplot(td, aes(x = date_time, y = sal, color = as.factor(tank), shape = coral_no)) +
   geom_point() +
-  theme(axis.text.x = element_text(angle = 90)) +
-  theme_bw(base_size = 20)
+  theme_bw(base_size = 20) +
+  theme(axis.text.x = element_text(angle = 90))
+  
 
 tank_plots <- ggarrange(temp, pH, DO, sal, common.legend = T, nrow = 2, ncol = 2)
 tank_plots
 
-ggsave(here("Output/tank_plots.pdf"), tank_plots, h = 6, w = 10)
+ggsave(here("Output/tank_plots.pdf"), tank_plots, h = 10, w = 10)
 
 #sunset at 18:38 so during sampling on 3/6
 #flow rates all over the place so residence times super unpredictable and weird
