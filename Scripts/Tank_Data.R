3e6d50476eec907d97c595ef6636296ad4e0d27e
Maya Powell <maya62928@gmail.com>
2026-03-09 16:08:49 -1000

    updating tank data and plots for initial sampling today

--- Scripts/Tank_Data.R
+++ Scripts/Tank_Data.R
@@ -14,36 +14,45 @@ library(lubridate)
 td <- read.csv(here("Data/tank_data_biofac.csv"))
 
 td <- td %>%
-  mutate(date_time = paste(date,pH_time))
-
+  mutate(date_time = paste(date,pH_time)) %>%
+  mutate(date_time = mdy_hm(date_time)) %>%
+  mutate(tank = as.factor(tank)) %>% 
+  dplyr::filter(treatment != "acclimation")
 
 #plot data
 
-temp <- ggplot(td, aes(x = date_time, y = temp, color = as.factor(tank), shape = coral_no)) +
+temp <- ggplot(td, aes(x = date_time, y = temp, color = treatment, group = tank)) +
   geom_point() +
+  geom_line() +
   theme_bw(base_size = 20) +
-  theme(axis.text.x = element_text(angle = 90))
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_x_datetime(date_labels = "%D %H")
 
-pH <- ggplot(td, aes(x = date_time, y = pH, color = as.factor(tank), shape = coral_no)) +
+pH <- ggplot(td, aes(x = date_time, y = pH, color = treatment, group = tank)) +
   geom_point() +
+  geom_line() +
   theme_bw(base_size = 20) +
-  theme(axis.text.x = element_text(angle = 90))
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_x_datetime(date_labels = "%D %H")
 
-DO <- ggplot(td, aes(x = date_time, y = DO, color = as.factor(tank), shape = coral_no)) +
+DO <- ggplot(td, aes(x = date_time, y = DO, color = treatment, group = tank)) +
   geom_point() +
+  geom_line() +
   theme_bw(base_size = 20) +
-  theme(axis.text.x = element_text(angle = 90))
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_x_datetime(date_labels = "%D %H")
 
-sal <- ggplot(td, aes(x = date_time, y = sal, color = as.factor(tank), shape = coral_no)) +
-  geom_point() +
-  theme_bw(base_size = 20) +
-  theme(axis.text.x = element_text(angle = 90))
+# sal <- ggplot(td, aes(x = date_time, y = sal, color = as.factor(tank), shape = coral_no)) +
+#   geom_point() +
+#   theme_bw(base_size = 20) +
+#   theme(axis.text.x = element_text(angle = 90))
   
 
-tank_plots <- ggarrange(temp, pH, DO, sal, common.legend = T, nrow = 2, ncol = 2)
+tank_plots <- ggarrange(temp, pH, DO, common.legend = T, nrow = 2, ncol = 2)
 tank_plots
 
 ggsave(here("Output/tank_plots.pdf"), tank_plots, h = 10, w = 10)
 
 #sunset at 18:38 so during sampling on 3/6
 #flow rates all over the place so residence times super unpredictable and weird
+#for initial tank data - much better!!
